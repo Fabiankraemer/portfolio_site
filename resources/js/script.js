2efd08c959ab71e1276b1e37ba7d928e76ee147c
Fabian Kraemer <63078076+Fabiankraemer@users.noreply.github.com>
2021-10-12 21:20:31 +0200

    add JS and links

---
+++ resources/js/script.js
@@ -0,0 +1,18 @@
+// Nav Interaction
+
+const aboutMe = document.getElementById('aboutme');
+const mail = document.getElementById('mail');
+const github = document.getElementById('github');
+const seeMore = document.getElementById('seemore');
+
+const recolor = function (event) {
+  event.target.style.color = 'rgb(100, 38, 31)';
+  setTimeout(function () {
+    event.target.style.color = '';
+  }, 500);
+};
+
+aboutMe.addEventListener('mouseover', recolor, false);
+mail.addEventListener('mouseover', recolor, false);
+github.addEventListener('mouseover', recolor, false);
+seeMore.addEventListener('mouseover', recolor, false);
